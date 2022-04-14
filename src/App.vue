73eb0a01a6c6b7ac6337fd4a9e1e9ec6a1c1864e
Lisa Bost <bost.lisam@gmail.com>
2022-04-14 13:43:52 -0500

    Created RavelrySearch component --Installed axios for ajax calls --Use AXIOS and AJAX to search the Ravelry API based on search term Added RavelrySearch component to App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <navigation-header></navigation-header>
+    <b-container>
+      <ravelry-search></ravelry-search>
+    </b-container>
+
 <!--    <nav>-->
 <!--      <router-link to="/">Home</router-link> |-->
 <!--      <router-link to="/about">About</router-link>-->
@@ -14,7 +18,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
@@ -33,7 +36,8 @@ nav a.router-link-exact-active {
 </style>
 <script>
 import NavigationHeader from "@/views/NavigationHeader";
+import RavelrySearch from "@/components/RavelrySearch";
 export default {
-  components: {NavigationHeader}
+  components: {RavelrySearch, NavigationHeader}
 }
 </script>
---
+++ src/components/RavelrySearch.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <b-form @submit.prevent="search" id="ravelry-search-form" class="mt-3">
+      <b-form-group id="search-term-input" label="Search Ravelry" label-for="search-term" description="Example search terms: hat, socks, blanket, etc.">
+        <b-form-input id="search-term" v-model="searchTerm" type="text" placeholder="Enter search term" required></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "RavelrySearch",
+  props: {},
+  data() {
+    return {
+      searchTerm: '',
+      searchResults: []
+    }
+  },
+  methods: {
+    search() {
+      if(this.searchTerm) {
+        this.$emit('searching');
+        // build request arguments
+        let url = 'https://api.ravelry.com/patterns/search.json';
+        let config = {
+          params: {
+            query: this.searchTerm,
+          },
+          auth: {
+            username: 'd94df3344302c08b7079c71041d90bbb',
+            password: '01-eZUS5Q6kykGn0bribecD3ARfRU8stkMuOVi2I'
+          }
+        }
+        // execute ajax request using promises
+        axios.get(url, config)
+            .then(response => {
+              if(response.data.patterns.length > 0) {
+                for (const i in response.data.patterns) {
+                  this.searchResults.push(response.data.patterns[i]);
+                }
+              } else {
+                this.searchResults = [];
+                this.$emit('no-search-results-found');
+              }
+            })
+            .catch(error => {
+              console.log('AJAX SEARCH ERROR', error);
+              // TODO: Put error message into toast for user
+            })
+            .finally(() => {
+              this.$emit('search-finished', this.searchResults)
+              console.log('Search Results: ' + this.searchResults);
+            })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
