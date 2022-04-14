62bd288a3de0da8ce8f2f228ee610c20a0cf33af
Lisa Bost <bost.lisam@gmail.com>
2022-04-14 14:00:39 -0500

    Created Pattern model

---
+++ src/models/Pattern.js
@@ -0,0 +1,14 @@
+function Pattern() {
+    this.free = false;
+    this.id = 0;
+    this.name = '';
+    this.permalink = '';
+    this.personal_attributes = '';
+    this.first_photo = {};
+    this.designer = {};
+    this.pattern_author = {};
+    this.pattern_sources = [];
+
+}
+
+export default Pattern;
