d11fe63428c377c1ed5b4cd3854b1a757e3434e3
Lisa Bost <bost.lisam@gmail.com>
2022-05-01 13:38:34 -0500

    Fixed display of more details when more details modal is clicked

--- src/components/MoreDetailsModal.vue
+++ src/components/MoreDetailsModal.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <b-button v-b-modal="modalId"><slot></slot></b-button>
-    <b-modal scrollable centered :title="pattern.name" :id="modalId" class="more-details-modal">
+    <b-modal scrollable centered :title="item.name" :id="modalId" class="more-details-modal">
       <template>
-        <p v-if="pattern.price > 0">Price: {{pattern.price}} {{pattern.currency}}</p>
+        <p v-if="item.price > 0">Price: {{item.price}} {{item.currency}}</p>
         <p>Get the Pattern:
-          <a :href="`https://ravelry.com/patterns/library/${item.permalink}`">{{pattern.name}}</a>
+          <a :href="`https://ravelry.com/patterns/library/${item.permalink}`">{{item.name}}</a>
         </p>
-        <p class="my-4" v-html="pattern.notes_html"></p>
+        <p class="my-4" v-html="item.notes_html"></p>
       </template>
     </b-modal>
   </div>
@@ -19,38 +19,12 @@
 
 export default {
   name: "MoreDetailsModal",
-  props: {pattern: {type: Object}},
+  props: {item: {type: Object}},
   data() {
     return {
     }
   },
   methods: {
-    // moreDetails() {
-    //   if(this.item.id) {
-    //     // build search arguments
-    //     let patternId = this.item.id;
-    //     let url = `https://api.ravelry.com/patterns/${patternId}.json`;
-    //     let config = {
-    //       auth: {
-    //         username: 'd94df3344302c08b7079c71041d90bbb',
-    //         password: '01-eZUS5Q6kykGn0bribecD3ARfRU8stkMuOVi2I'
-    //       }
-    //     }
-    //     // execute ajax request
-    //     axios.get(url, config)
-    //           .then(response => {
-    //             if(response != null) {
-    //               this.pattern = Object.assign(new Pattern, response.data.pattern)
-    //             }
-    //             else {
-    //               this.pattern = new Pattern();
-    //             }
-    //           })
-    //           .catch(error => {
-    //             console.log('Error finding pattern', error)
-    //           });
-    //   }
-    // }
   },
   computed: {
     modalId() {
